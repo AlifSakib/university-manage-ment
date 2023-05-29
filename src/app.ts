2033d518324a3002118a552b255b55e06face563
alifsakib <alifsakib@gmail.com>
2023-05-29 07:09:03 +0600

    [DONE] setupeslint, prettier, husky, lint-staged

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,11 @@ import express, { Application, Request, Response } from 'express'
 
 const app: Application = express()
 
-            app.use(cors())
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Application is running 🚀')
 })
 
